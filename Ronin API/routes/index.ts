10b50382b1db7e371ecda58584dd823a0f5cda79
Zcross091 <126247344+Zcross091@users.noreply.github.com>
2026-07-05 12:31:55 +0530

    Delete Ronin API directory

--- Ronin API/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance, RegisterOptions } from 'fastify';
-
-import gogoanime from './gogoanime';
-
-const routes = async (fastify: FastifyInstance) => {
-  await fastify.register(gogoanime);
-};
-
-export default routes;
