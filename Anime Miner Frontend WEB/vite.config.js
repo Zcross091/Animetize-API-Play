e2e2e7e953221808b505155b254d1080158a254c
Zcross091 <126247344+Zcross091@users.noreply.github.com>
2026-07-09 12:27:05 +0000

    new vite

--- Anime Miner Frontend WEB/vite.config.js
+++ Anime Miner Frontend WEB/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Animetize-API-Play/',
+  base: './', // 👈 From '/Animetize-API-Play/' to './'
 })
